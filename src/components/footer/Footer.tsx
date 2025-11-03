21af232833773b6e2fe1a3a64dab608bd1ef8b80
Vasin Oleksii <vasin.oleksii@gmail.com>
2025-11-03 20:08:50 +0100

    just add author

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,17 @@
+import Typography from "@mui/material/Typography/Typography";
+import Stack from "@mui/material/Stack/Stack";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Stack direction="row" spacing={2} alignItems="center">
+      <Typography fontStyle="italic" my="6px">
+        Vasin Oleksii
+      </Typography>{" "}
+      <Typography fontStyle="italic" my="6px">
+        {new Date().getFullYear()} ©
+      </Typography>{" "}
+    </Stack>
+  );
 };
 
 export default Footer;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,12 @@ import { Typography } from "@mui/material";
 const Header = () => {
   return (
     <header>
-      <Typography component="h1" color="textPrimary" fontSize="20px">
+      <Typography
+        component="h1"
+        color="textPrimary"
+        fontSize="3rem"
+        fontStyle="italic"
+      >
         Cyrillic-Converter
       </Typography>
     </header>
